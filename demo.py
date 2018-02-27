8faaf43424141a5f4e3948ef600aa6e993795239
rajveermalviya <rajveer0malviya@gmail.com>
2018-02-27 21:51:44 +0530

    Enter

--- demo.py
+++ demo.py
@@ -1,19 +1,21 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'PASTE_CUSTOMER_KEY_HERE'
-consumer_secret = 'PASTE_CONSUMER_SECRET_HERE'
+consumer_key = input('\n\nPASTE_CUSTOMER_KEY_HERE\n')
+consumer_secret = input('\n\nPASTE_CONSUMER_SECRET_HERE\n')
 
-access_token = 'PASTE_ACCESS_TOKEN_HERE'
-access_token_secret = 'PASTE_ACCESS_TOKEN_SECRET_HERE'
+access_token = input('\n\nPASTE_ACCESS_TOKEN_HERE\n')
+access_token_secret = input('\n\nPASTE_ACCESS_TOKEN_SECRET_HERE\n')
+
+print('\n\n')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-userstring=input('Enter the Keyword\n')
-tweet_count=int(input('Enter no. of Tweets to check\n'))
+userstring=input('\n\nEnter the Keyword\n')
+tweet_count=int(input('\n\nEnter no. of Tweets to check\n'))
 public_tweets = api.search(userstring,count=tweet_count)
 
 for tweet in public_tweets:
