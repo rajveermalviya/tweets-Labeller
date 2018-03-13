42d6f1e937b59894a6c3d431ed45d63475f4b565
Rajveer Malviya <36819268+rajveermalviya@users.noreply.github.com>
2018-03-13 20:06:08 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open('tweetLabeller').then(function(cache) {
       return cache.addAll(
         [
           '/index.html'
@@ -9,3 +9,28 @@ self.addEventListener('install', function(event) {
     })
   );
 })
+document.querySelector('.cache-article').addEventListener('click', function(event) {
+  event.preventDefault();
+  var id = this.dataset.articleId;
+  caches.open('tweetLabeller-' + id).then(function(cache) {
+    fetch('/get-article-urls?id=' + id).then(function(response) {
+      // /get-article-urls returns a JSON-encoded array of
+      // resource URLs that a given article depends on
+      return response.json();
+    }).then(function(urls) {
+      cache.addAll(urls);
+    });
+  });
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('tweetLabeller').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
