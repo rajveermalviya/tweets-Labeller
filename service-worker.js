379cdd163887636c43499a5a7d8b0bae495c55f2
Rajveer Malviya <36819268+rajveermalviya@users.noreply.github.com>
2018-03-13 19:15:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
+importScripts('polyfill.js');
+self.addEventListener('install', function(e) {
 
+    e.waitUntil(caches.open('tweets-Labeller').then(function(cache) {
+
+        return cache.addAll(['/', '/index.html', '/index.html?homescreen=1', '/?homescreen=1', ]);
+
+    }));
+
+});
