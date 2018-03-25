3e106b573cf2f85686bf79d8655f0ed69158014a
Rajveer Malviya <36819268+rajveermalviya@users.noreply.github.com>
2018-03-25 15:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,35 +10,28 @@ def sw():
     sw_file = open(file='static/service-worker.js',mode='r')
     return Response(response=sw_file,mimetype="text/javascript")
 
-
-@app.route('/fucked/files/lol')
-def gcloud():
-    cred_file = open(file='static/app_label.json', mode='r')
-    return Response(response=cred_file, mimetype="application/json")
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return redirect("https://tweets-labeller.herokuapp.com/")
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect("https://tweets-labeller.herokuapp.com/")
 
 
-# @app.errorhandler(403)
-# def forbidden(e):
-#     return redirect("https://tweets-labeller.herokuapp.com/")
+@app.errorhandler(403)
+def forbidden(e):
+    return redirect("https://tweets-labeller.herokuapp.com/")
 
 
-# @app.errorhandler(410)
-# def gone(e):
-#     return redirect("https://tweets-labeller.herokuapp.com/")
+@app.errorhandler(410)
+def gone(e):
+    return redirect("https://tweets-labeller.herokuapp.com/")
 
 
-# @app.errorhandler(500)
-# def internal_error(e):
-#     return redirect("https://tweets-labeller.herokuapp.com/")
+@app.errorhandler(500)
+def internal_error(e):
+    return redirect("https://tweets-labeller.herokuapp.com/")
 
 @app.route('/', methods = ["POST"])
 def form():
@@ -51,4 +44,4 @@ def form():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
